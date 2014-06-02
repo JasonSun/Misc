6ea3748d6a4c25cd29baa28dd8916f28344ada17
Yang <ysunbj@yahoo.com>
2014-06-02 12:34:22 +0800

    Hash的时间效率不如Array的效率高

--- Codeforces/5C-hash.rb
+++ Codeforces/5C-hash.rb
@@ -1,5 +1,7 @@
 # # 使用Hash表保存)对应的最长的sequence长度
-# # 使用Hash会超时，不能通过，可见Hash的时间效率不如直接用数组快。
+
+# # 使用Hash会超时提交不过，报超时错误，可见Hash的时间效率不如直接用数组快。
+
 line = gets
 line.strip!
 hash_longest = Hash.new  # )对应的最长的sequence长度
